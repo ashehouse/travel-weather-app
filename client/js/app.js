efeb06d5049aa28d700bdbe7cc23731a3bc960b3
Jody-Dixon <jodymdixon@gmail.com>
2020-07-31 09:58:58 -0400

    feat: Added initial API to Geonames
    Started the funcationality of the app with a fetch call to
    the Geonames API.
    
    I also added the styly directories to the git tracking.

---
+++ client/js/app.js
@@ -0,0 +1,16 @@
+const geonamesApiURL = 'http://api.geonames.org/searchJSON?q=london&fuzzy=0.8&username=ashehouse'
+
+const getLonLat = fetch(geonamesApiURL)
+.then(response => response.json())
+.catch(e => requestError(e, 'Thing Failed'));
+
+
+const getWeather = async (apiUrl) => {
+    const response = await fetch(apiUrl);
+    try {
+    const data = await response.json();
+    return data;
+    } catch(error) {
+        console.log('error', error);
+    };
+}
